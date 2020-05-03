74cda7e3f5d96b84ea1a78a19c264d8364b61c89
m1khah <alexeyenko92@gmail.com>
2020-05-03 18:05:26 +0300

    Added first test

--- src/test/java/org/example/pages/HomePage.java
+++ src/test/java/org/example/pages/HomePage.java
@@ -3,11 +3,26 @@ package org.example.pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 
     private WebDriver driver;
 
+    private static String URL="https://www.ranorex.com/web-testing-examples/vip/";
+    public static String CONTENT_TITLE = "VIP Database Test Web Application";
+    public static String FIRST_NAME = "Kir";
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+        driver.get(URL);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "Content")
+    public WebElement contentTitle;
+
+    public String checkContentTitleText(){
+        return contentTitle.getText();
+    }
 }
--- src/test/java/org/example/tests/HomePageTest.java
+++ src/test/java/org/example/tests/HomePageTest.java
@@ -1,29 +1,44 @@
 package org.example.tests;
 
+import static org.example.pages.HomePage.CONTENT_TITLE;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.example.pages.HomePage;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.example.pages.HomePage;
 
 
-public class HomePageTest
-{
+public class HomePageTest {
 
     private WebDriver driver;
 
-    @Test
-    public void openHomepage() {
-
+    @Before
+    public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-
-
-}
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
     }
+
+    @Test
+    public void openHomepage() {
+        HomePage homePage = new HomePage(driver);
+        assertEquals(true, homePage.checkContentTitleText().equals(CONTENT_TITLE));
+    }
+
+    @Test
+    public void fillFirstName() {
+
+    }
+
+    @After
+    public void quit(){
+        driver.quit();
+    }
+
 }
